f586895c50093f3eba2b0ebb42fec540930abb56
Anas Abuelhaag <86550384+at2nothing@users.noreply.github.com>
2024-05-12 08:08:25 +0300

    after thought

--- Hangman Game/Form1.Designer.cs
+++ Hangman Game/Form1.Designer.cs
@@ -176,6 +176,7 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hangman";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Shown += new System.EventHandler(this.Form1_Shown);
